dd0e1cabf076520cd4bd5000c0fc17c3b52d1bac
Mijail Rondon <mijail@hey.com>
2020-06-27 10:27:29 -0500

    Add build script

---
+++ build-book.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Generate docx for Kindle Create
+docker run -v $PWD:/source jagregory/pandoc \
+  -o aws-saa-preparation.docx --toc --toc-depth=1 title.txt \
+  introduction.md \
+  elastic-beanstalk.md \
+  dynamodb.md
+
+# Generate EPUB
+docker run -v $PWD:/source jagregory/pandoc \
+  -o aws-saa-preparation.epub --toc --toc-depth=1 title.txt \
+  introduction.md \
+  elastic-beanstalk.md \
+  dynamodb.md
+
