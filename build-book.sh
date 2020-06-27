78f84a867fda777c379c87601ca103f75b0dbdd0
Mijail Rondon <mijail@hey.com>
2020-06-27 16:26:03 -0500

    Generate EPUB and PDF files

--- build-book.sh
+++ build-book.sh
@@ -2,21 +2,19 @@
 
 set -e
 
-# Generate docx for Kindle Create
-docker run -v $PWD:/source jagregory/pandoc \
-  -o aws-saa-preparation.docx --toc --toc-depth=1 title.txt \
-  introduction.md \
+CHAPTERS="introduction.md \
   elastic-beanstalk.md \
   dynamodb.md \
   cloud-watch.md \
-  cloud-trail.md
+  cloud-trail.md \
+  "
+
+# Generate PDF
+docker run -v $PWD:/source jagregory/pandoc \
+  -o aws-saa-preparation.pdf --toc --toc-depth=1 title.txt \
+  $CHAPTERS
 
 # Generate EPUB
 docker run -v $PWD:/source jagregory/pandoc \
   -o aws-saa-preparation.epub --toc --toc-depth=1 title.txt \
-  introduction.md \
-  elastic-beanstalk.md \
-  dynamodb.md \
-  cloud-watch.md \
-  cloud-trail.md
-
+  $CHAPTERS
